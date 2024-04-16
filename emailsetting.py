768d7ab2c802e137e480ad2b786220c285baac03
juhi <110763165+juhi79@users.noreply.github.com>
2024-04-16 14:19:44 +0530

    Add files via upload

---
+++ emailsetting.py
@@ -0,0 +1,15 @@
+SET_EMAIL_USE_TLS = True
+SET_EMAIL_HOST='smtp.gmail.com'
+SET_EMAIL_HOST_USER='dskreddy02@gmail.com'
+SET_EMAIL_HOST_PASSWORD=''
+SET_EMAIL_PORT=587
+SET_EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+SET_DEFAULT_FROM_EMAIL = 'shivakrishnareddy.si4@gmail.com'
+
+
+
+#SET_EMAIL_HOST_USER='chennaisunday2@gmail.com'
+#SET_EMAIL_HOST_PASSWORD='customerservices'
+#SET_EMAIL_PORT=587
+#SET_EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+#SET_DEFAULT_FROM_EMAIL = 'chennaisunday.cs0216@gmail.com'
